5c98b2308e5db1d7421afe38e72d051b9a2965a5
Kirill <kiron19951995@gmail.com>
2023-10-09 19:31:00 +0300

    Complete home work version 2.2

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -3,89 +3,80 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int storageSize;
-    private boolean isPresentUuid;
 
-    public void clear() {
-        Arrays.fill(storage, null);
-        storageSize = 0;
-    }
-
-    public void update(Resume r, String uuid) {
-        for (int i = 0; i < storageSize; i++) {
-            if (storage[i].getUuid().equals(r.getUuid())) {
-                r.setUuid(uuid);
-                storage[i] = r;
+    private int findIndex(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
             }
         }
-        isPresent(uuid);
+        return -1;
+    }
+
+    private void printErrorMessage(String uuid) {
+        System.err.printf("Error: %s is not found \n", uuid);
     }
 
     public void save(Resume r) {
         if (size() == storage.length) {
             System.err.println("Error: Storage is full");
         }
-        boolean isFoundValue = false;
-        if (!Objects.isNull(r)) {
-            for (int i = 0; i < storageSize; i++) {
-                if (storage[i].getUuid().equals(r.getUuid())) {
-                    System.err.println("Error: The value is present in the list " + r.getUuid());
-                    isFoundValue = true;
-                    break;
-                }
-            }
-            if (!isFoundValue) storage[storageSize++] = r;
+        int index = findIndex(r.getUuid());
+        if (index == -1) {
+            storage[storageSize++] = r;
+        } else {
+            System.err.println("Error: The value is present in the list " + r.getUuid());
+        }
+    }
+
+    public void update(Resume r, String uuid) {
+        int index = findIndex(r.getUuid());
+        if (index != -1) {
+            storage[index].setUuid(uuid);
+        } else {
+            printErrorMessage(uuid);
         }
     }
 
     public Resume get(String uuid) {
-        if (!Objects.isNull(uuid)) {
-            for (int i = 0; i < storageSize; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    isPresentUuid = true;
-                    return storage[i];
-                }
-            }
+        int index = findIndex(uuid);
+        if (index != -1) {
+            return storage[index];
+        } else {
+            printErrorMessage(uuid);
         }
-        isPresent(uuid);
         return null;
     }
 
-    public void delete(String uuid) {
-        for (int i = 0; i < storageSize; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[storageSize - 1];
-                storage[storageSize - 1] = null;
-                storageSize--;
-                isPresentUuid = true;
-                break;
-            }
-        }
-        isPresent(uuid);
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, storageSize);
+    }
 
+    public void delete(String uuid) {
+        int index = findIndex(uuid);
+        if (index != -1) {
+            storage[index] = storage[storageSize - 1];
+            storage[storageSize - 1] = null;
+            storageSize--;
+        } else {
+            printErrorMessage(uuid);
+        }
     }
 
     public int size() {
         return storageSize;
     }
 
-    private void isPresent(String uuid) {
-        if (!isPresentUuid) {
-            System.err.printf("Error: %s is not found \n", uuid);
-        }
+    public void clear() {
+        Arrays.fill(storage, null);
+        storageSize = 0;
     }
 }
