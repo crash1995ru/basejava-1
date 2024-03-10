a3682a2fc0daf09e1ac09a2dcb0808ec0b8689e2
crash1995ru <kiron19951995@gmail.com>
2024-03-10 11:34:36 +0300

    final commit 5 lessons

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
-    private Map<String, Resume> map = new HashMap<>();
+    private final Map<String, Resume> map = new HashMap<>();
 
     @Override
     protected Resume getSearchKey(String uuid) {
@@ -46,7 +46,7 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     public Resume[] getAll() {
-        return map.values().toArray(new Resume[map.size()]);
+        return map.values().toArray(new Resume[0]);
     }
 
 
